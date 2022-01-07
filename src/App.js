4fe72d0724732f3a0ef0645a1ea4cd9f8de5b08a
Jesse Sandvik <sandvikjesse@gmail.com>
2022-01-07 15:19:27 -0500

    basic outline done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink } from "react-router-dom";
 
-function App() {
+import "./styles/global/global.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="template">
+        <div className="top">
+          <div className="one"></div>
+          <div className="two"></div>
+          <div className="three">
+        <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/"
+                >
+                  home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                >
+                  about
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/portfolio"
+                >
+                  portfolio
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                >
+                  contact
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        </div>
+        <div className="mid">
+          <div className="one">
+            <div className="i x"></div>
+            <div className="i y">
+            </div>
+          </div>
+          <div className="two"></div>
+          <div className="three">
+            <div className="x">
+            <i className="fab fa-instagram-square"></i>
+            <i className="fab fa-twitter-square"></i>
+            <i className="fab fa-facebook-square"></i>
+            <i className="fas fa-envelope-square"></i>
+            </div>
+            <div className="y"></div>
+          </div>
+        </div>
+        <div className="bot">
+          <div className="one">
+            <p>&copy; Body Designs Studio 2022</p>
+          </div>
+          <div className="two"></div>
+          <div className="three"></div>
+        </div>
+      </div>
     </div>
   );
 }
