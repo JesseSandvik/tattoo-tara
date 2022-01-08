ce88ad0287aaa0625fbc702c84c447cfc48480c5
Jesse Sandvik <sandvikjesse@gmail.com>
2022-01-07 22:51:52 -0500

    added styling

--- src/organisms/Main.js
+++ src/organisms/Main.js
@@ -2,7 +2,16 @@ import "../styles/main/main.css";
 
 const Main = () => {
     return (
-        <main></main>
+        <main>
+            <div className="item"></div>
+            <div className="item two"></div>
+            <div className="item four">
+                <div className="box">
+                    <h1>Miss Tattoo Tara</h1>
+                </div>
+            </div>
+            <div className="item"></div>
+        </main>
     );
 }
 
